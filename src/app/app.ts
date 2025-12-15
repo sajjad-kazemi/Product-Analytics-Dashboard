fcb6305247eb2d7a2f9bbd0ed254837dc3bf29ac
Saji <sajjadkazemiswe@gmail.com>
2025-12-15 18:14:49 +0330

    change standalone project to modular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   template: `
     <h1>Welcome to {{title}}!</h1>
 
     <router-outlet />
   `,
-  styles: [],
+  standalone: false,
+  styles: []
 })
 export class App {
-  protected title = 'Front-End-Challenge';
+  protected title = 'Product-Analytics-Dashboard';
 }
